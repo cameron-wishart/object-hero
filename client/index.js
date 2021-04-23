fd47326f1033eb2bc6e34db859fba9d5fc41f509
camo20112 <camo20112@gmail.com>
2021-04-23 15:43:47 +0100

    basic movement

--- client/index.js
+++ client/index.js
@@ -1,10 +1,53 @@
 const socket = io('ws://localhost:3000')
 
+//colors
+BG_COLOR = '#c2c2c2'
+PLAYER_COLOR = '#231f20'
+
+//canvas context
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+let keyPresses = {};
+
 socket.on('init', handleInit)
-socket.on('player disconnect', (msg) => {
+socket.on('players', (msg) => {
     console.log(msg)
 })
+socket.on('tick', (msg) => {
+    paintGame(msg)
+})
+
+window.addEventListener('keydown', keyDownListener);
+function keyDownListener(event) {
+    keyPresses[event.key] = true;
+    console.log(event.key)
+    socket.emit("keypress", event.key)
+}
+
+window.addEventListener('keyup', keyUpListener);
+function keyUpListener(event) {
+    keyPresses[event.key] = false;
+}
+
 function handleInit(msg) {
-    socket.emit("hello", "world")
+    socket.emit("hello", msg)
     console.log(msg)
+}
+
+function paintGame(state) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    paintPlayers(state.players)
+}
+
+function paintPlayers(players) {
+    console.log(players)
+    for (let player in players) {
+        ctx.fillStyle = BG_COLOR
+        ctx.fillRect(players[player].x, players[player].y, 10, 10)
+    }
+}
+
+function handleGame(state) {
+    requestAnimationFrame(state)
 }
--- server/index.js
+++ server/index.js
@@ -4,18 +4,45 @@ const io = require('socket.io')(http, {
     cors: { origin: '*' }
 })
 
+const state = {
+    players:
+    {
+
+    }
+    ,
+    map:
+        [
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+        ]
+}
+
 io.on('connection', socket => {
     console.log('a user connected');
-    socket.emit('init', 'hello')
-    socket.on('hello', msg => {
-        console.log(msg)
+    state.players[socket.id] = { x: 10, y: 10 }
+    socket.emit('init', state.players)
+
+    socket.on('keypress', () => {
+        state.players[socket.id].x += 1;
     })
 
+    startGameInterval(socket, state)
 
-    // socket.on('disconnecting', (obj) => {
-    //     socket.brodcast.emit('player disconnect', 'player gone!')
-    // })
 
+    socket.on('disconnect', () => {
+        delete state.players[socket.id]
+        console.log('Client disconnected')
+    });
 })
+
+function startGameInterval(socket, state) {
+    const interval = setInterval(() => {
+        socket.emit('tick', state)
+    }, 1000 / 20)
+}
+
+
 
 http.listen(3000, () => console.log('listening on http://localhost:3000'))
